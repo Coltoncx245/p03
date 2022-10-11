5c848dc40285bd9bd127ff80a23cc3c87bf4800a
Coltoncx245 <30328876+Coltoncx245@users.noreply.github.com>
2022-10-11 09:49:14 -0500

    Added output to file and minor fixes

--- p03/p03.cpp
+++ p03/p03.cpp
@@ -1,6 +1,18 @@
+/*
+Author:			Colton Cox
+Student ID:		20511024
+Course:			CMSC 2123 - Discrete Structures
+CRN:			10748, Fall, 2022
+Project:		p03
+Due:			October 12, 2022
+Account:		tt001
+*/
+
 #include <fstream>
 #include <iostream>
 #include <math.h>
+#include <iomanip>
+#include <cstring>
 
 
 int ef0(int n)
@@ -28,10 +40,39 @@ int ef0(int n)
 
 int af0(int n) { return 2 * (pow(2, n)) + 4*n + 4; }
 
-std::string parseFile(std::string fileName)
+
+int main(int argc, char* argv[])
 {
-	std::ifstream inputFile = std::ifstream(fileName);
+	char inputFileName[255];
+	char outputFileName[255];
+
+	switch (argc) 
+	{
+	case 1:
+		std::cout << "Enter the input file name: ";
+		std::cin >> inputFileName;
+		std::cout << "Enter the output file name: ";
+		std::cin >> outputFileName;
+		break;
+
+	case 2:
+		strcpy(inputFileName, argv[1]);
+		std::cout << "Enter the output file name: ";
+		std::cin >> outputFileName;
+		break;
+	
+	case 3:
+		strcpy(inputFileName, argv[1]);
+		strcpy(outputFileName, argv[2]);
+		break;
+	}
+		
+
+	std::ifstream inputFile = std::ifstream(inputFileName);
+	std::ofstream outputFile = std::ofstream(outputFileName);
 	std::string result = "";
+
+	outputFile << "Code Fragment 3\nN" << std::setw(13) << "Analytical" << std::setw(14) << "Empirical\n";
 
 	while (inputFile)
 	{
@@ -39,17 +80,10 @@ std::string parseFile(std::string fileName)
 
 		while (inputFile >> w)
 		{
-			std::cout << ef0(w - '0') << "\t" << af0(w-'0') << "\n";
+			outputFile << w << std::setw(13) << ef0(w - '0') << std::setw(13) << af0(w - '0') << "\n";
 		}
 	}
 
-	return result;
-}
 
-int main()
-{
-
-	parseFile("i03.dat");
-	// std::cout << ef0(5);
 	return 0;
 }
