ee173ebb05cec2deb3c7ca7b33b6ef3b5579d245
Colton Cox <coltoncx245@gmail.com>
2024-10-15 13:08:46 -0500

    Lazy_tree framework

---
+++ src/Lazy_tree.h
@@ -0,0 +1,79 @@
+#include "utility.h"
+
+template <class Record>
+class Lazy_tree: public Search_tree<Record>
+{
+public:
+    bool insert(const Record &data);
+    bool remove(const Record &data);
+    int findMin();
+    int findMax();
+    bool constains(const Record &data);
+    std::string print();
+    int height();
+    int size();
+
+
+protected:
+    bool search_and_insert(Binary_node<Record>* &sub_root, const Record &new_data);
+    bool search_and_delete(Binary_node<Record>* &sub_root, const Record &target);
+    Binary_node<Record>* search_for_node(Binary_node<Record>* sub_root, const Record &target) const;
+
+};
+
+// INSERT IMPLEMENTATION
+template <class Record>
+bool Lazy_tree<Record>::insert(const Record &data)
+{
+
+}
+
+//REMOVE IMPLEMENTATION
+template <class Record>
+bool Lazy_tree<Record>::remove(const Record &data)
+{
+
+}
+
+// FIND MIN IMPLEMENTATION
+template <class Record>
+int Lazy_tree<Record>::findMin()
+{
+
+}
+
+// FIND MAX IMPLEMENTATION
+template <class Record>
+int Lazy_tree<Record>::findMax()
+{
+
+}
+
+// CONSTAINS IMPLEMENTATION
+template <class Record>
+bool Lazy_tree<Record>::constains(const Record &data)
+{
+
+
+}
+
+// PRINT IMPLEMENTATION
+template <class Record>
+std::string Lazy_tree<Record>::print()
+{
+
+}
+
+// HEIGHT IMPLEMENTATION
+template <class Record>
+int Lazy_tree<Record>::height()
+{
+
+}
+
+// SIZE IMPLEMENTATION
+template <class Record>
+int Lazy_tree<Record>::size()
+{
+
+}
