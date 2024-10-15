18e2ac1393001fca8edbf5789b73a1c83c610956
zenOfBass <108999864+zenOfBass@users.noreply.github.com>
2024-10-15 14:06:32 -0500

    Added insert and search and insert

--- src/Lazy_tree.h
+++ src/Lazy_tree.h
@@ -26,7 +26,18 @@ protected:
 template <class Record>
 bool Lazy_tree<Record>::insert(const Record &data)
 {
-
+    if (this->root == nullptr)
+    {
+        // Create a new node with the data
+        this->root = new Binary_node<Record>();
+        ++count;
+        return true;
+    }
+    else
+    {
+        // Use search and insert to handle recursion
+        return false;
+    }
 }
 
 //REMOVE IMPLEMENTATION
@@ -85,4 +96,26 @@ template <class Record>
 int Lazy_tree<Record>::size()
 {
 
+}
+
+template <class Record>
+bool Lazy_tree<Record>::search_and_insert(Binary_node <Record>* &sub_root, const Record &new_data)
+{
+    if (sub_root == nullptr)
+    {
+        sub_root = new Binary_node<Record>(new_data);
+        ++ count;
+        return true;
+    }
+    else if (new_data < sub_root->data)
+    {
+        return search_and_insert(sub_root->left, new_data);
+    }
+    else if (sub_root->deleted)
+    {
+        // If the node is marked as deleted, we undelete it
+        sub_root->deleted = false;
+        return true;
+    }
+    else return false; // Data already exists and is not marked as deleted
 }
