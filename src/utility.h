d79da7201943fd70d8a1e090f183e9e37f2247db
zenOfBass <108999864+zenOfBass@users.noreply.github.com>
2024-10-15 19:53:05 -0500

    Added stdexcept.h

--- src/utility.h
+++ src/utility.h
@@ -3,9 +3,10 @@
 #include <sstream>
 #include <fstream>
 #include <stack>
+#include <stdexcept>
 
 using namespace std;
 
 enum Error_code { duplicate_error, not_present, success };
 
-int string_to_int(string s);
+int string_to_int(const string& s);
